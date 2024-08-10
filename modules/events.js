15c172e8bfc201f7fee38456c525a73bb2aeb8d6
Abdurasul09 <ahmedovabdurasul19@gmai.com>
2024-08-10 15:14:51 +0200

    events

---
+++ modules/events.js
@@ -0,0 +1,14 @@
+const Events = require("events");
+
+class Logger extends Events {
+  log(a, b) {
+    this.emit("calculator", a + b);
+  }
+}
+
+const logger = new Logger();
+logger.on("calculator", (data) => {
+  console.log(data);
+});
+
+logger.log(3,4)
