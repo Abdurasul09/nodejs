c50b4dd249fcaa7b0048f422150c0a5b9ea3dd5f
Abdurasul09 <ahmedovabdurasul19@gmai.com>
2024-08-10 15:37:14 +0200

    nodemon

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const http = require('http')
 const server = http.createServer((req, res) => {
     console.log(req.url);
     res.write("<h1>Hello world</h1>")
+    res.write("<h2>Hello world</h2>")
+    res.write("<h3>Hello world</h3>")
+    res.write("<h4>Hello world</h4>")
     res.end()
 })
 
